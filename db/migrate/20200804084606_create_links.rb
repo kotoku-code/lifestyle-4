8f9d0043422ebee70015987b4f54850e7e50ca4e
kotoku-code <kennedy.kotoku@gmail.com>
2020-08-04 10:50:26 +0200

    added links controller and model

---
+++ db/migrate/20200804084606_create_links.rb
@@ -0,0 +1,11 @@
+class CreateLinks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :url
+      t.text :description
+      t.belongs_to :user, foreign_key: true, index: true
+      t.timestamps
+    end
+  end
+end
