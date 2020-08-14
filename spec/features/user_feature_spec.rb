97d006b18a25b468da85cdc1a39833203be78e66
kotoku kennedy <kennedy.kotoku@gmail.com>
2020-08-15 00:15:35 +0200

    working on negative validation test

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'signup page', type: :feature do
-  scenario 'signup page' do
+  before(:each) do
     visit new_user_registration_path
     fill_in 'Username', with: 'maggie'
     fill_in 'Email', with: 'kennedy@gmail.com'
@@ -11,4 +11,22 @@ RSpec.describe 'signup page', type: :feature do
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully')
   end
+
+  
+  scenario 'çreating an article' do
+    visit new_link_path
+    expect(page).to have_content('Lifestyles')
+  end
+
+  # scenario 'çreating an article with invalid inputs' do
+  #   visit new_link_path
+  #   fill_in 'Title', with 'Test Title'
+  #   fill_in 'url', with 'url-link'
+  #   fill_in 'description', with 'Hello there'
+
+  #   click_button 'Çreate Link'
+
+  #   expect(page).to have_content('Úrl is invalid')
+  # end
+
 end
