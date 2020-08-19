37cf343a2692f460ccfa259963f18525a1ab49c6
kotoku kennedy <kennedy.kotoku@gmail.com>
2020-08-19 14:47:34 +0200

    secret key

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module LifestyleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.secret_key_base = 'blipblapblup'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
